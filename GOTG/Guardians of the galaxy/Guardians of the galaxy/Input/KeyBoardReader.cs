88e2717554878c3ddcbafa46704b541637d7d406
nick <s114623@ap.be>
2020-12-01 17:35:38 +0100

    added a testSprite

--- GOTG/Guardians of the galaxy/Guardians of the galaxy/Input/KeyBoardReader.cs
+++ GOTG/Guardians of the galaxy/Guardians of the galaxy/Input/KeyBoardReader.cs
@@ -22,12 +22,15 @@ namespace Guardians_of_the_galaxy.Input
         {
             var direction = Vector2.Zero;
             KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.Left))
-                direction = new Vector2(-1, 0);
-            if (state.IsKeyDown(Keys.Right))
-                direction = new Vector2(1, 0);
-            if (state.IsKeyDown(Keys.Space))
-                direction = new Vector2(0, -10);
+            if (state.IsKeyDown(Keys.Left)||state.IsKeyDown(Keys.Q))
+                direction = new Vector2(-2, 0);
+
+            if (state.IsKeyDown(Keys.Right) || state.IsKeyDown(Keys.D))
+                direction = new Vector2(2, 0);
+
+            if (state.IsKeyDown(Keys.Space) || state.IsKeyDown(Keys.Z))
+                direction = new Vector2(0, -30);
+
 
             return direction;
         }
