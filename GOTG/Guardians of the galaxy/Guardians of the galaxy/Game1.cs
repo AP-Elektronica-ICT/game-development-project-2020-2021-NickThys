72145b704f777aa047df4d70ffc38252b622a1d7
nick <s114623@ap.be>
2020-12-07 11:59:09 +0100

    Added music to the game

--- GOTG/Guardians of the galaxy/Guardians of the galaxy/Game1.cs
+++ GOTG/Guardians of the galaxy/Guardians of the galaxy/Game1.cs
@@ -3,8 +3,10 @@ using Guardians_of_the_galaxy.Input;
 using Guardians_of_the_galaxy.Sprites;
 using Guardians_of_the_galaxy.WorldDesign;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -17,6 +19,7 @@ namespace Guardians_of_the_galaxy
         private SpriteBatch _spriteBatch;
         private Level _level; 
         private List<sprite> _sprites;
+        private Song _song;
 
         int windowWidth = 990;
         int windowHeight = 792;
@@ -27,6 +30,7 @@ namespace Guardians_of_the_galaxy
             _graphics.PreferredBackBufferHeight = windowHeight;
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+            MediaPlayer.IsRepeating = true;
         }
 
         protected override void Initialize()
@@ -57,13 +61,16 @@ namespace Guardians_of_the_galaxy
                         Space=Keys.Space,
                         
 
-                    },Position=new Vector2(100,100),speed=5f
+                    },Position=new Vector2(66,200),speed=5f
                     
                 }
             };
             // TODO: use this.Content to load your game content here
             _sprites.AddRange(_level.getBlocks());
-            
+            _song = Content.Load<Song>("Come and Get Your Love [8 Bit Tribute to Redbone & Guardians of the Galaxy] - 8 Bit Universe");
+            MediaPlayer.Volume = 0.5f;
+
+            MediaPlayer.Play(_song);
         }
 
         private void InitializeGameObjects()
@@ -78,6 +85,7 @@ namespace Guardians_of_the_galaxy
 
             foreach (var sprite in _sprites)
             {
+                
                 sprite.Update(gameTime, _sprites);
             }
             // TODO: Add your update logic here
