83f8a2a0e894a74b1c8db658eb8909b9db3803bb
nick <s114623@ap.be>
2021-01-26 10:00:36 +0100

    Created globals class

---
+++ GOTG/Guardians of the galaxy/Guardians of the galaxy/Globals.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians_of_the_galaxy
+{
+    class Globals
+    {
+        public static int WindowWidth, WindowHeight;
+        public static SpriteBatch SpriteBatch;
+        public static ContentManager ContentLoader;
+    }
+}
